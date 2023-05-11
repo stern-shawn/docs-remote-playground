2d3290cefe826f39030a530adff1716c23e40a3c
Shawn Stern <sstern@twilio.com>
2023-05-11 13:45:00 -0700

    feat: integrate Paste and map html -> Paste components, mostly...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@code-hike/mdx/dist/index.css';
+import { Theme } from '@twilio-paste/core/theme';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Theme.Provider theme="Twilio">
+      <Component {...pageProps} />{' '}
+    </Theme.Provider>
+  );
 }
